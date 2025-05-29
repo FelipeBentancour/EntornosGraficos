3ad7882360f2d86682ece6594e05b2f2f8d2124c
Felipe <bentancourfelipe@gmail.com>
2025-05-29 09:33:00 -0300

    Realizar Ejercicio2 de arrays en PHP

---
+++ Practica4_PHP/Ejercicio7/ejercicio2A.php
@@ -0,0 +1,12 @@
+<?php
+// Se crea un array asociativo llamado $matriz con dos elementos:
+// La clave "x" tiene el valor "bar"
+// La clave 12 tiene el valor true
+$matriz = array("x" => "bar", 12 => true);
+
+// Imprime el valor asociado a la clave "x", que es "bar"
+echo $matriz["x"];
+
+// Imprime el valor asociado a la clave 12, que es true
+// En PHP, true al imprimirse se muestra como "1"
+echo $matriz[12];
